3c50afc22d5eca9beb3ca29a57f6118ae60ec77a
duy <duy123galaxy13@gmail.com>
2020-04-05 22:11:18 +0700

    Initial Commit

---
+++ GameForKids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameForKids
+//
+//  Created by Lê Duy on 4/5/20.
+//  Copyright © 2020 Lê Duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
